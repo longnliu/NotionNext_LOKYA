f95a136b5868373e2ade6fe30cde76c3bfbf2440
longnliu <82907864+longnliu@users.noreply.github.com>
2025-10-02 18:55:45 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Mr.Lokya', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '独立设计师✏️AI·平面·3D', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-lokya.vercel.app', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://lokya.dpdns.org/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '设计素材,AIGC,海报模板,', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
